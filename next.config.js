26baa0d34b5d1ed59b20b3f2af6a5f19821ec334
YohanesVitoMA <yohanes.vito@ti.ukdw.ac.id>
2025-12-21 05:39:51 +0700

    feat: add turbopack configuration to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  turbopack: {},
   webpack: (config, { isServer }) => {
     // Add verbose error logging
     config.stats = 'verbose';
